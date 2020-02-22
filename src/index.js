f84e53f8cd255ad01b441341ed89afafa54388f7
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-02-21 18:30:52 -0800

    added styled-components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import Routes from './routes';
 
 import './config/ReactotronConfig';
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#1C749C" />
+      <Routes />
+    </>
+  );
 }
 
 export default App;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { View } from 'react-native';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Main() {
-  return <View />;
+  return <Container />;
 }
