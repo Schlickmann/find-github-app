8b544889dc97f8471e169de4b8bd1bbf7f9bff87
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-02-21 19:44:14 -0800

    added flatlist to show users

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,7 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
-import { Container, Form, Input, SubmitButton } from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
 
 export default function Main() {
   const [newUser, setNewUser] = useState('');
@@ -46,6 +58,21 @@ export default function Main() {
           <Icon name="add" size={20} color="#fff" />
         </SubmitButton>
       </Form>
+
+      <List
+        data={usernames}
+        keyExtractor={user => user.login}
+        renderItem={({ item }) => (
+          <User>
+            <Avatar source={{ uri: item.avatar }} />
+            <Name>{item.name}</Name>
+            <Bio>{item.bio}</Bio>
+            <ProfileButton onPress={() => {}}>
+              <ProfileButtonText>See Profile</ProfileButtonText>
+            </ProfileButton>
+          </User>
+        )}
+      />
     </Container>
   );
 }
