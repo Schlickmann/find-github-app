fc182913ac4a964dea3056bbddbea828b0a7bce5
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-02-22 08:06:24 -0800

    added starred list

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,21 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default function User({ navigation, route }) {
-  console.tron.log(route.params);
-  return <View />;
+  const [stars, setStarts] = useState([]);
+
+  const { user } = route.params;
+
+  useEffect(() => {
+    async function loadUsersInfo() {
+      navigation.setOptions({ title: user.name });
+
+      const response = await api.get(`/users/${user.login}/starred`);
+
+      setStarts(response.data);
+    }
+
+    loadUsersInfo();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <Avatar source={{ uri: user.avatar }} />
+        <Name>{user.name}</Name>
+        <Bio>{user.bio}</Bio>
+      </Header>
+
+      <Stars
+        data={stars}
+        keyExtractor={star => String(star.id)}
+        renderItem={({ item }) => (
+          <Starred>
+            <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+            <Info>
+              <Title>{item.name}</Title>
+              <Author>{item.owner.login}</Author>
+            </Info>
+          </Starred>
+        )}
+      />
+    </Container>
+  );
 }
 
 User.propTypes = {
   navigation: PropTypes.shape({
-    // navigate: PropTypes.func,
+    setOptions: PropTypes.func,
   }).isRequired,
   route: PropTypes.shape({
     params: PropTypes.shape({
-      user: PropTypes.shape({}),
+      user: PropTypes.shape({
+        name: PropTypes.string,
+        login: PropTypes.string,
+        avatar: PropTypes.string,
+        bio: PropTypes.string,
+      }),
     }),
   }).isRequired,
 };
